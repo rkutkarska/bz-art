68081d15641c26ea07b2799ea848ec64a714ffc3
Ralena Kutkarska <ralena.dimitrova@gmail.com>
2023-01-30 14:52:24 +0200

    Save item to firestore db

--- src/components/CreateItem.js
+++ src/components/CreateItem.js
@@ -2,23 +2,25 @@ import React from "react";
 import { useState } from "react";
 import { storage, db } from "../Firebase";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { collection, addDoc } from "firebase/firestore";
 import { v4 } from "uuid";
 import "../styles/CreateItem.css";
 
 export const CreateItem = () => {
 
     const initialFormData = Object.freeze({
-        'item-name': '',
-        'item-category': '',
-        'item-material': '',
-        'item-description': '',
-        'item-price': 0,
-        'item-discount': 0,
-        'item-url': ''
+        'name': '',
+        'category': '',
+        'material': '',
+        'description': '',
+        'price': 0,
+        'discount': 0,
+        'url': ''
     });
 
-    const [formData, updateFormData] = React.useState(initialFormData);
+    const [formData, updateFormData] = useState(initialFormData);
     const [imageUpload, setImageUpload] = useState(null);
+    const itemCollectionRef = collection(db, "items");
 
     const handleChange = (e) => {
         updateFormData({
@@ -28,18 +30,24 @@ export const CreateItem = () => {
         });
     }
 
-    const saveItem = (e) => {
+    const saveItem = async (e) => {
         e.preventDefault();
-        console.log(formData);
-        // console.log(e.target)
-        if (imageUpload == null) { alert('Неуспешно качване!'); return; }
+
+        if (imageUpload == null) {
+            // alert('Неуспешно качване!');
+            return;
+        }
         const imageRef = ref(storage, `images/items/${v4() + imageUpload.name}`);
         uploadBytes(imageRef, imageUpload).then(() => {
-            alert('Файлът е качен успешно!');
+            // alert('Файлът е качен успешно!');
             getDownloadURL(imageRef).then((url) => {
-                console.log(url);
+                addDoc(itemCollectionRef, { ...formData, url });
             });
         });
+
+
+        // clear form after submit
+        e.target.reset();
     };
 
     const clearImage = () => {
@@ -51,41 +59,40 @@ export const CreateItem = () => {
             <div className="form-container">
                 <h1>Добавяне на продукт</h1>
                 <form onSubmit={saveItem} className="form">
-                    <label htmlFor="item-name">Име</label>
-                    <input onChange={handleChange} type="text" name="item-name" />
-                    <label htmlFor="item-category">Категория</label>
-                    <select onChange={handleChange} name="item-category" defaultValue={'DEFAULT'}>
+                    <label htmlFor="name">Име</label>
+                    <input onChange={handleChange} type="text" name="name" />
+                    <label htmlFor="category">Категория</label>
+                    <select onChange={handleChange} name="category" defaultValue={'DEFAULT'}>
                         <option value="DEFAULT" disabled={true}>--Моля изберете --</option>
                         <option value="ring">Пръстен</option>
                         <option value="ring">Пръстен</option>
                         <option value="ring">Пръстен</option>
                     </select>
-                    <label htmlFor="item-material">Материал</label>
-                    <select onChange={handleChange} type="select" name="item-material" defaultValue={'DEFAULT'}>
+                    <label htmlFor="material">Материал</label>
+                    <select onChange={handleChange} type="select" name="material" defaultValue={'DEFAULT'}>
                         <option value="DEFAULT" disabled={true}>--Моля изберете --</option>
                         <option value="gold">Злато</option>
                         <option value="gold">Злато</option>
                         <option value="gold">Злато</option>
                     </select>
-                    <label htmlFor="item-description">Описание</label>
-                    <textarea onChange={handleChange} name="item-description" cols="30" rows="3"></textarea>
+                    <label htmlFor="description">Описание</label>
+                    <textarea onChange={handleChange} name="description" cols="30" rows="3"></textarea>
                     <div className="flex-items">
                         <div className="flex-items__item">
-                            <label htmlFor="item-price">Цена</label>
-                            <input onChange={handleChange} type="number" name="item-price" />
+                            <label htmlFor="price">Цена</label>
+                            <input onChange={handleChange} type="number" name="price" />
                             <p>BGN</p>
                         </div>
                         <div className="flex-items__item">
-                            <label htmlFor="item-discount">Намаление</label>
-                            <input onChange={handleChange} type="number" name="item-discount" />
+                            <label htmlFor="discount">Намаление</label>
+                            <input onChange={handleChange} type="number" name="discount" />
                             <p>BGN</p>
                         </div>
                     </div>
                     <label htmlFor="images" className="drop-container">
                         <span className="drop-title">Провлачете снимка тук</span>
                         или
-                        <input type="file" onChange={(e) => { setImageUpload(e.target.files[0]) }} id="images" accept="image/*" required name="item-url" />
-                        {/* <button onClick={uploadImage}>Качи</button> */}
+                        <input type="file" onChange={(e) => { setImageUpload(e.target.files[0]) }} id="images" accept="image/*" required name="url" />
                         <button onClick={clearImage}>Премахни</button>
                     </label>
                     <input type="submit" className="button yellow" name="" value="Запиши" />
