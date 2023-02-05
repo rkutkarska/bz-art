c4d0ade14a6bf7c4058871873004030c54423856
rkutkarska <ralena.dimitrova@gmail.com>
2023-02-05 21:00:40 +0200

    Add spinner to CreateCategory component

--- src/components/CreateCategory.js
+++ src/components/CreateCategory.js
@@ -2,6 +2,9 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import { collection, addDoc, getDocs } from "firebase/firestore";
 import { db } from "../Firebase";
+import { Spinner } from "./spinner/Spinner";
+
+import * as categoriesService from '../services/categoriesService';
 
 import "../styles/CreateCategory.css";
 
@@ -14,20 +17,16 @@ export const CreateCategory = () => {
 
     const [formData, updateFormData] = useState(initialFormData);
     const [categories, setCategories] = useState([]);
+    let [loading, setLoading] = useState(true);
+
     const categoriesCollectionRef = collection(db, "categories");
 
-    const fetchCategories = async () => {
-
-        await getDocs(categoriesCollectionRef)
-            .then((res => {
-                const data = res.docs
-                    .map((doc) => ({ ...doc.data(), id: doc.id }));
-                setCategories(data);
-            }))
-    }
-
     useEffect(() => {
-        fetchCategories();
+        setTimeout(function () {
+            categoriesService.getAll()
+                .then(categories => setCategories(categories));
+            setLoading(false);
+        }, 1000);
     }, []);
 
     const handleChange = (e) => {
@@ -35,25 +34,42 @@ export const CreateCategory = () => {
             ...formData,
             [e.target.name]: e.target.value.trim()
         })
-        console.log({ ...formData });
     }
 
     const saveCategory = (e) => {
         e.preventDefault();
 
-        if (categories
-            .map(x => (x.name.toLowerCase()))
-            .includes((formData.name).toLowerCase())) {
+        if (categoriesService.checkIfExist(categories, formData)) {
             alert('Категорията не е записана, защото вече има такава!');
             return;
         } else {
             addDoc(categoriesCollectionRef, { ...formData, dateCreated: new Date() });
+            categoriesService.getAll()
+                .then(categories => setCategories(categories));
+
+            e.target.reset();
+        }
+    };
+
+    const renderList = (categories) => {
+        if (categories.length === 0) {
+            return (<span>Все още няма добавени категории...</span>);
         }
 
-        fetchCategories().then(() => {
-            e.target.reset();
-        });
-    };
+        return categories.map((category) => (<li className="category-list__item" key={category.id}>{category.name}</li>));
+    }
+
+    let categoriesList;
+
+    if (loading) {
+        categoriesList = <Spinner />;
+    } else {
+        categoriesList = (
+            <ul className="category-list ul-clear" name="categories">
+                {renderList(categories)}
+            </ul>
+        );
+    }
 
     return (
         <div className="container">
@@ -62,13 +78,8 @@ export const CreateCategory = () => {
                 <form onSubmit={saveCategory} onChange={(e) => handleChange(e)} className="form">
                     <label htmlFor="name">Име</label>
                     <input type="text" name="name" placeholder="Име на категория" required />
-
                     <label className="existing-categories">Налични категории: </label>
-                    {
-                        categories.length !== 0
-                            ? <ul className="category-list ul-clear" name="categories">{categories.map((category) => (<li className="category-list__item" key={category.id}>{category.name}</li>))}</ul>
-                            : <span>Все още няма добавени категории...</span>
-                    }
+                    {categoriesList}
                     <input type="submit" className="button yellow" value="Запиши" />
                 </form>
             </div>
