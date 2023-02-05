b2284b1e1e9c2f1e7347444c2f48332525cf706f
rkutkarska <ralena.dimitrova@gmail.com>
2023-02-05 20:57:58 +0200

    Add another firebase config

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,14 +2,25 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from '@firebase/firestore';
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCC41XTcg4GsnXwcMaCuBqU15fJ5SV57GU",
+//   authDomain: "bz-art.firebaseapp.com",
+//   databaseURL: "https://bz-art-default-rtdb.firebaseio.com",
+//   projectId: "bz-art",
+//   storageBucket: "bz-art.appspot.com",
+//   messagingSenderId: "375146892569",
+//   appId: "1:375146892569:web:40361651c0c0a41f57c988"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCC41XTcg4GsnXwcMaCuBqU15fJ5SV57GU",
-  authDomain: "bz-art.firebaseapp.com",
-  databaseURL: "https://bz-art-default-rtdb.firebaseio.com",
-  projectId: "bz-art",
-  storageBucket: "bz-art.appspot.com",
-  messagingSenderId: "375146892569",
-  appId: "1:375146892569:web:40361651c0c0a41f57c988"
+  apiKey: "AIzaSyCd2DZepiGQowhKMmL7ivRHzB7OIsflQzQ",
+  authDomain: "wings-rln.firebaseapp.com",
+  databaseURL: "https://wings-rln-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "wings-rln",
+  storageBucket: "wings-rln.appspot.com",
+  messagingSenderId: "990526634300",
+  appId: "1:990526634300:web:fd605c622e63ec79f7c639"
+
 };
 
 const app = initializeApp(firebaseConfig);
