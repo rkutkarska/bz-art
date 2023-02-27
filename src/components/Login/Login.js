a2776cd3738cd9453b5eea4710ac078aa35fb745
Ralena Kutkarska <ralena.dimitrova@gmail.com>
2023-02-27 18:02:56 +0200

    Add login and register components

---
+++ src/components/Login/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../../styles/LoginRegister.css";
+
+export const Login = () => {
+    return (
+        <div className="login-container">
+            <div className="login-container__image"></div>
+            <div className="login-container__form">
+                <h1>Вписване</h1>
+                <form class="form">
+                    <label htmlFor="username">Потребител</label>
+                    <input type="text" name="username" />
+                    <label htmlFor="password">Парола</label>
+                    <input type="password" name="password" />
+                    <label className="remember-me" htmlFor="remember-me"><input type="checkbox" /> Запомни ме</label>
+                    <input className="button yellow" type="submit" name="" value="Вход" />
+                </form>
+                <p className="inline">Нямате акаунт? </p>
+                <a href="/register">Регистрирайте се</a>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/Register/Register.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../../styles/LoginRegister.css";
+
+export const Register = () => {
+    return (
+        <div class="register-container">
+            <div className="register-container__image"></div>
+            <div className="register-container__form">
+                <h1>Регистрация</h1>
+                <form className="form">
+                    <label htmlFor="username">Потребител</label>
+                    <input type="text" name="username" />
+                    <label htmlFor="password">Парола</label>
+                    <input type="password" name="password" />
+                    <label htmlFor="repeat-password">Повторете паролата</label>
+                    <input type="password" name="repeat-password" />
+                    <input className="button yellow" type="submit" value="Регистрирай ме" />
+                </form>
+                <p className="inline">Вече имате акаунт? </p>
+                <a href="/login">Вход</a>
+            </div>
+        </div>
+    );
+}
