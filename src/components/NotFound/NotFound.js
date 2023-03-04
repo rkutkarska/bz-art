d7773b824fc25a14b6a715443c85e4eef3afee3d
rkutkarska <ralena.dimitrova@gmail.com>
2023-03-04 13:19:02 +0200

    Add 404 eror page

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+import styles from './NotFound.module.css';
+
+export const NotFound = () => {
+    return (
+        <div>
+            <img class={styles["img-center"]} src={require("../../img/404/404.png")} />
+            <h1>Page not found!</h1>
+            <div class={styles["text-center"]}>
+                <p>Ами сега? Страницата, която търсиш не съществува!</p>
+                <Link to="/" class="button purple">Към началната страница</Link>
+            </div>
+        </div>
+    )
+}
