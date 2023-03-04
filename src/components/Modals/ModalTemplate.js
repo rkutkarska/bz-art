05af12c0600a16ccb0afd896ec8a3d09c3c6007e
rkutkarska <ralena.dimitrova@gmail.com>
2023-03-04 13:20:12 +0200

    Add modal template component

---
+++ src/components/Modals/ModalTemplate.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import styles from './ModalTemplate.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { regular } from '@fortawesome/fontawesome-svg-core/import.macro';
+// modal type, modal message
+
+export const ModalTemplate = (props) => {
+    let buttons;
+    let modalCaption = '';
+    const [isClosed, setIsClosed] = useState(true);
+
+    const onClose = () => {
+        if (isClosed) {
+            setIsClosed(false);
+        } else {
+            setIsClosed(true);
+        }
+    }
+
+    if (props.obj.type === 'alert') {
+        modalCaption = 'Предупреждение!'
+        buttons = (<>
+            <button className="button green">Да</button>
+            <button className="button red">Не</button>
+        </>);
+    }
+
+    if (props.obj.type === 'error') {
+        modalCaption = 'Грешка!'
+        buttons = (
+            <button onClick={onClose} className="button blue">OK</button>
+        );
+    }
+
+    if (props.obj.type === 'information') {
+        modalCaption = 'Информация'
+
+        buttons = (<>
+            <button onClick={onClose} className="button blue">OK</button>
+        </>);
+    }
+
+    if (props.obj.type === 'confirm') {
+        modalCaption = 'Моля, потвърдете...'
+        buttons = (<>
+            <button className="button green">Да</button>
+            <button className="button red">Не</button>
+        </>);
+    }
+
+    return (
+        <>
+            {
+                isClosed
+                    ? <>
+                        <div className={styles["modal-background"]}></div>
+                        <div className={styles["modal-container"]}>
+                            <div className={styles.modal}>
+                                <button
+                                    onClick={onClose}
+                                    className={`${styles.button} ${styles.close}`}>
+                                    <FontAwesomeIcon icon={regular('circle-xmark')} className={styles.icon} />
+                                </button>
+                                <p className={styles[`modal__` + props.obj.type]}>{modalCaption}</p>
+                                <p className={styles["modal__message"]}>{props.obj.message}</p>
+                                <div className={styles["modal__buttons"]}>
+                                    {buttons}
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                    : null
+            }
+        </>
+
+    );
+}
--- src/services/itemsService.js
+++ src/services/itemsService.js
@@ -1,18 +1,45 @@
-import { db } from "../Firebase";
-import { collection, doc, getDoc } from "firebase/firestore";
-
+import { db, storage } from "../Firebase";
+import { collection, doc, getDoc, addDoc } from "firebase/firestore";
+import { v4 } from "uuid";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 export const getItem = async (id) => {
     try {
         const itemsCollectionRef = doc(db, "items", id);
         const docSnap = await getDoc(itemsCollectionRef);
-        if(docSnap.exists()) {
+        if (docSnap.exists()) {
             return docSnap.data();
         } else {
-            console.log("Document does not exist!")
+            // TODO modal
+            // setModalObject({ message: "Document does not exist!", type: "error" });
+            return;
         }
 
-    } catch(error) {
-        console.log(error)
+    } catch (error) {
+        // TODO modal
+        // setModalObject({ message: error, type: "error" });
     }
 };
+
+export const saveItem = async (e, itemImageUpload, itemsData, setIsModalOpen, setModalObject) => {
+    e.preventDefault();
+
+    const imageRef = ref(storage, `images/items/${v4() + itemImageUpload.name}`);
+
+    if (itemImageUpload === '') {
+        e.target.value = '';
+        setModalObject({ message: 'Изображението не е качено!', type: 'error' });
+        return
+    }
+
+    await uploadBytes(imageRef, itemImageUpload).then(() => {
+        getDownloadURL(imageRef).then((url) => {
+            const itemsCollectionRef = collection(db, 'items');
+            addDoc(itemsCollectionRef, { ...itemsData, imageUrl: url, dateCreated: new Date() });
+            setIsModalOpen(true);
+            setModalObject({ message: 'Продуктът е добавен успешно!', type: 'information' });
+        });
+    });
+
+    e.target.reset();
+}
