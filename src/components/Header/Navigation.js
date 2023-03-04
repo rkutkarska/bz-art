5f3097cb2dd8bb60b1250a4c0be6d55f89e26b71
rkutkarska <ralena.dimitrova@gmail.com>
2023-03-04 14:23:12 +0200

    Add NavLink active styles

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -1,13 +1,18 @@
 import React from "react";
-import {Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export const Navigation = () => {
+
+    const setNavStyle = ({ isActive }) => {
+        return isActive ? 'active-link' : undefined;
+    }
+
     return (
         <nav className="navigation">
             <ul className="navigation__links ul-clear">
-                <li><Link to="/">Начало</Link></li>
-                <li><Link to="/items">Артикули</Link></li>
-                <li><Link to="/contacts">Контакти</Link></li>
+                <li><NavLink className={setNavStyle} to="/">Начало</NavLink></li>
+                <li><NavLink className={setNavStyle} to="/items">Артикули</NavLink></li>
+                <li><NavLink className={setNavStyle} to="/contacts">Контакти</NavLink></li>
             </ul>
         </nav>
     );
