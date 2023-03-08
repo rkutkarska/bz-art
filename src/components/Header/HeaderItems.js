d1981fbce7892c1de1b796392e4533ec46364f7e
rkutkarska <ralena.dimitrova@gmail.com>
2023-03-08 23:03:22 +0200

    Update header items component

--- src/components/Header/HeaderItems.js
+++ src/components/Header/HeaderItems.js
@@ -4,12 +4,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 export const HeaderItems = () => {
+
+    function handleLogOut() {
+
+    }
+
     return (
         <div className="header__items">
             <div className="header__buttons">
-                <Link className="button yellow" to="/login"><FontAwesomeIcon icon={solid('user')} className="fa-icon" />Вход</Link>
-                <Link className="button yellow" to="/logout"><FontAwesomeIcon icon={solid('user')} className="fa-icon" />Изход</Link>
-                <Link className="button yellow" to="/cart"><FontAwesomeIcon icon={solid('cart-shopping')} className="fa-icon" />Количка</Link>
+                <button className="button yellow" to="/login"><FontAwesomeIcon icon={solid('user')} className="fa-icon" />Вход</button>
+                <button onClick={handleLogOut} className="button yellow" to="/logout"><FontAwesomeIcon icon={solid('user')} className="fa-icon" />Изход</button>
+                <button className="button yellow" to="/cart"><FontAwesomeIcon icon={solid('cart-shopping')} className="fa-icon" />Количка</button>
             </div>
             <Link to="/"><img className="logo" src={require('../../img/logo.png')} alt="logo" /></Link>
             <div className="header__search-bar">
