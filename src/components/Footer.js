55f9fe30059857a18bae7f1c1650fbd588538a6f
Ralena Kutkarska <ralena.dimitrova@gmail.com>
2023-03-08 15:08:13 +0200

    Fix footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import '../styles/Footer.css';
 
 export const Footer = () => {
     return (
-        <>
+        <footer>
             <svg
                 className="wave"
                 xmlns="http://www.w3.org/2000/svg"
@@ -17,13 +17,13 @@ export const Footer = () => {
                     d="M0,96L48,101.3C96,107,192,117,288,138.7C384,160,480,192,576,176C672,160,768,96,864,69.3C960,43,1056,53,1152,74.7C1248,96,1344,128,1392,144L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
                 ></path>
             </svg>
-            <footer>
+            <div>
                 <p>&copy; Studio BZ Art 2023</p>
                 <p>Programming & Design: Ralena Kutkarska</p>
                 <p>Made with &nbsp;
                     <FontAwesomeIcon icon={solid('heart')} className="heart"></FontAwesomeIcon>
                 </p>
-            </footer>
-        </>
+            </div>
+        </footer>
     );
 }
