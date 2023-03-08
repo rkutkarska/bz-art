b41c08aac23949ee898646e2f3133f21b0f6f5c9
Ralena Kutkarska <ralena.dimitrova@gmail.com>
2023-03-08 15:11:36 +0200

    Add conditional rendering in item details
    For new and sale items

--- src/components/ItemDescription/ItemDescription.js
+++ src/components/ItemDescription/ItemDescription.js
@@ -15,37 +15,48 @@ export const ItemDescription = () => {
     }, [])
 
     return (
-        <div class="container">
+        <div className="container">
             <h1>{item.type} "{item.name}"</h1>
-            <div class="item">
-                <img class="item__image" src={item.imageUrl} alt="jewellery" />
-                <div class="item__description">
-                    <div class="details">
+            <div className="item">
+                <img className="item__image" src={item.imageUrl} alt="jewellery" />
+                <div className="item__description">
+                    <div className="details">
                         <h2>Категория</h2>
                         <p>{item.categoryName}</p>
                         <h2>Материал</h2>
                         <p>{item.material}</p>
                         <h2>Описание</h2>
                         <p>{item.description}</p>
-                        <h2>Цена</h2>
-                        <p>{item.price} лв.</p>
+                        {item.discount > 0
+                            ? <>
+                                <h2>Цена</h2>
+                                <p><s>{item.price}</s> лв.</p>
+                                <p>{item.price - item.discount} лв.</p>
+                                <h2>Отстъпка</h2>
+                                <p>{`${((item.discount / item.price) * 100).toFixed(0)} %`}</p>
+                            </>
+                            : <>
+                                <h2>Цена</h2>
+                                <p>{item.price} лв.</p>
+                            </>
+                        }
                         <h2>Наличност</h2>
                         <p>{item.quantity} бр.</p>
                     </div>
 
-                    <div class="add-cart">
+                    <div className="add-cart">
                         <div>
                             <h2>Количество</h2>
                             <input type="number" min="1" defaultValue={1} />
                         </div>
-                        <Link class="button yellow same-size" to="">
+                        <Link className="button yellow same-size" to="">
                             <FontAwesomeIcon icon={solid('cart-shopping')} className="fa-icon" />Добави в количката
                         </Link>
                     </div>
 
                     <div className="favourites">
                         <h2>Харесва ли ти?</h2>
-                        <Link class="button purple same-size" to="">
+                        <Link className="button purple same-size" to="">
                             <FontAwesomeIcon icon={solid('heart')} className="fa-icon" />
                             Добави в любими
                         </Link>
