0d20510e7a782584d8e405bf98edb8836f36c446
Ralena Kutkarska <ralena.dimitrova@gmail.com>
2023-03-08 15:13:24 +0200

    Add background to 404 page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,12 +5,12 @@ import styles from './NotFound.module.css';
 
 export const NotFound = () => {
     return (
-        <div>
+        <div class="container">
             <img className={styles["img-center"]} src={require("../../img/404/404.png")} alt="404 Page not found!" />
             <h1>Page not found!</h1>
             <div className={styles["text-center"]}>
                 <p>Ами сега? Страницата, която търсиш не съществува!</p>
-                <Link to="/" class="button purple">Към началната страница</Link>
+                <Link to="/" className="button purple">Към началната страница</Link>
             </div>
         </div>
     )
