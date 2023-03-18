fed4874654a9edabfd22cf9838af4d996dd65cb8
rkutkarska <ralena.dimitrova@gmail.com>
2023-03-18 21:54:00 +0200

    Add delete functionality for all documents

--- src/services/adminService.js
+++ src/services/adminService.js
@@ -1,5 +1,7 @@
 import { storage, db } from "../Firebase";
-import { collection, getDocs, addDoc, where, query, orderBy } from "firebase/firestore";
+import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc, where, query, orderBy } from "firebase/firestore";
+import { refFromURL } from "firebase/storage";
+
 // import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 // import { v4 } from "uuid";
 
@@ -15,7 +17,52 @@ export const getAllItemsOrdered = async (documentType, documentSortType) => {
 
     const [param, order] = documentSortType.split(', ');
     const q = query(collection(db, documentType), orderBy(param, order));
-    return await getDocs(q);
 
+    return await getDocs(q);
     // where("name", "==", true));
 };
+
+function deleteRow(id, documents, setDocuments) {
+    let copy = [...documents];
+    copy = copy.filter((document) => document.id != id)
+    setDocuments(copy);
+}
+
+export const deleteItem = async (id, documents, setDocuments, isClicked) => {
+    const itemDoc = doc(db, "items", id);
+    try {
+        const response = await deleteDoc(itemDoc).then(() => true);
+        if (response) {
+            deleteRow(id, documents, setDocuments);
+            isClicked.current = true;
+        }
+    } catch (error) {
+        // TODO modal -> error.message error.code;
+    }
+}
+
+export const deleteCategory = async (id, documents, setDocuments, isClicked) => {
+    const categoryDoc = doc(db, "categories", id);
+    try {
+        const response = await deleteDoc(categoryDoc).then(() => true);
+        if (response) {
+            deleteRow(id, documents, setDocuments)
+            isClicked.current = true;
+        }
+    } catch (error) {
+        // TODO modal -> error.message error.code;
+    }
+}
+
+export const deleteMaterial = async (id, documents, setDocuments, isClicked) => {
+    const materialDoc = doc(db, "materials", id);
+    try {
+        const response = await deleteDoc(materialDoc).then(() => true);
+        if (response) {
+            deleteRow(id, documents, setDocuments)
+            isClicked.current = true;
+        }
+    } catch (error) {
+        // TODO modal -> error.message error.code;
+    }
+}
