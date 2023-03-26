ae8b35522eb4118d069debc9d5c27e6db4917d20
rkutkarska <ralena.dimitrova@gmail.com>
2023-03-26 22:12:55 +0300

    Add modal to logout

--- src/components/Header/HeaderItems.js
+++ src/components/Header/HeaderItems.js
@@ -1,32 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import { useAuth } from '../../context/AuthContext';
+import { ModalTemplate } from '../Modals/ModalTemplate';
 
 export const HeaderItems = () => {
     const { currentUser } = useAuth();
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     const { logout } = useAuth();
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalObject, setModalObject] = useState({});
 
     async function handleLogOut() {
         try {
             await logout();
-            navigate("/login");
-
+            // navigate("/login");
         } catch (error) {
-            // console.log(Неуспешно отписване!)
-            // TODO modal
+            setIsModalOpen(true);
+            setModalObject({ message: 'Неуспешно отписване!', type: 'error' });
         }
     }
 
     return (
+
         <div className="header__items">
+            {isModalOpen ? <ModalTemplate obj={{ modalObject, setIsModalOpen }} /> : false}
             <div className="header__buttons">
                 {!currentUser && <Link className="button yellow" to="/login"><FontAwesomeIcon icon={solid('user')} className="fa-icon" />Вход</Link>}
                 {currentUser && <Link onClick={handleLogOut} className="button yellow"><FontAwesomeIcon icon={solid('user')} className="fa-icon" />Изход</Link>}
                 {currentUser && <><Link className="button yellow" to="/cart"><FontAwesomeIcon icon={solid('cart-shopping')} className="fa-icon" />Количка</Link>
-                <span className="items-count">5</span></> }
+                    <span className="items-count">5</span></>}
                 {/* TODO read from db item count in the cart */}
             </div>
             <Link to="/"><img className="logo" src={require('../../img/logo.png')} alt="logo" /></Link>
