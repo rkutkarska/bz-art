c3215a5f51d700add18ca16de3dfb9a32e5191dd
Ralena Kutkarska <ralena.dimitrova@gmail.com>
2023-03-27 16:23:19 +0300

    Add ReadMaterial component

--- src/components/AdminActions/CrudDocuments/ReadMaterial/ReadMaterial.js
+++ src/components/AdminActions/CrudDocuments/ReadMaterial/ReadMaterial.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { Spinner } from '../../../Spinner/Spinner';
+import * as materialsService from '../../../../services/materialsService';
+
+import styles from './ReadMaterial.module.css';
 
 export const ReadMaterial = () => {
-  return (
-    <div>ReadMaterial</div>
-  )
+    const [material, setMaterial] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const { materialId } = useParams();
+
+    useEffect(() => {
+        materialsService.getMaterial(materialId)
+            .then((material) => {
+                setMaterial(material)
+                setIsLoading(false);
+            });
+    }, [])
+
+    return (
+        <div className={`${styles.read} container`}>
+            {
+                isLoading
+                    ? <Spinner />
+                    : <>
+                        <h1>Преглед на материал</h1>
+                        <div className="form-container">
+                            <form className={styles["materials-form"]}>
+                                <label>Име:</label>
+                                <input type="text" defaultValue={material.materialName} disabled />
+                                <Link to="/crud-documents" className="button red">Затвори</Link>
+                            </form>
+                        </div>
+                    </>
+            }
+        </div>
+    )
 }
--- src/services/materialsService.js
+++ src/services/materialsService.js
@@ -1,5 +1,5 @@
 import { db } from "../Firebase";
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import { collection, getDocs, getDoc, addDoc, doc } from "firebase/firestore";
 
 const materialsCollectionRef = collection(db, "materials");
 
@@ -16,3 +16,21 @@ export const saveMaterial = (e, materialsData) => {
     addDoc(materialsCollectionRef, { ...materialsData, dateCreated: new Date() });
     e.target.previousSibling.value = '';
 }
+
+export const getMaterial = async (id) => {
+    try {
+        const materialCollectionRef = doc(db, "materials", id);
+        const docSnap = await getDoc(materialCollectionRef);
+        if (docSnap.exists()) {
+            return docSnap.data();
+        } else {
+            // TODO modal
+            // setModalObject({ message: "Document does not exist!", type: "error" });
+            return;
+        }
+
+    } catch (error) {
+        // TODO modal
+        // setModalObject({ message: error, type: "error" });
+    }
+};
