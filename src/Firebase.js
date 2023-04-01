7056177bd0ad7d0b472ea59a229de8a7155f18ae
rkutkarska <ralena.dimitrova@gmail.com>
2023-04-01 15:01:06 +0300

    Add firebase configuration from .env file

--- src/Firebase.js
+++ src/Firebase.js
@@ -6,6 +6,12 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
 	// Your web app's Firebase configuration
+	apiKey: process.env.REACT_APP_FIREBASE_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
