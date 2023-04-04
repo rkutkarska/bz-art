86869b7ab1191fb85b5e86291027b504299e7170
rkutkarska <ralena.dimitrova@gmail.com>
2023-04-04 07:08:35 +0300

    Fix Auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -10,6 +10,7 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState();
+    const [loading, setLoading] = useState(true);
 
     const value = {
         currentUser,
@@ -41,6 +42,7 @@ export function AuthProvider({ children }) {
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user);
+            setLoading(false);
         });
 
         return unsubscribe;
@@ -48,7 +50,7 @@ export function AuthProvider({ children }) {
 
     return (
         <AuthContext.Provider value={value} >
-            {children}
+            {!loading && children}
         </AuthContext.Provider>
     )
 }
