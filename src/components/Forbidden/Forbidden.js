798d333db15efcb3f014cbc953a26862a352ed23
Ralena Kutkarska <ralena.dimitrova@gmail.com>
2023-04-05 17:29:58 +0300

    Add 403 forbidden component

---
+++ src/components/Forbidden/Forbidden.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Forbidden = () => {
+    return (
+        <div className="container">
+            {/* TODO style this component */}
+            <h2>403: Forbiden! You are unauthorized!</h2>
+        </div>
+    );
+}
