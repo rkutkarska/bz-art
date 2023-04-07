4477dd34d6aa3bd65ed51dfd82acec14aa88336f
Ralena Kutkarska <ralena.dimitrova@gmail.com>
2023-04-07 16:54:27 +0300

    Fix route guard

--- src/components/RequireAuth/RequireAuth.js
+++ src/components/RequireAuth/RequireAuth.js
@@ -1,33 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import { getUserRole } from '../../services/usersService';
+import { Forbidden } from '../Forbidden/Forbidden';
 
 // TODO get currentUserRole
 export const RequireAuth = ({ allowedRoles }) => {
-    const { currentUser } = useAuth();
-    const [currentUserRole, setCurrentUserRole] = useState('');
 
-    let userId = '';
-    if (currentUser) {
-        userId = currentUser.uid;
+    const user = useRef(null);
+    const [isUserLoaded, setIsUserLoaded] = useState(false);
+
+    const role = useRef(null);
+    const [isRoleLoaded, setIsRoleLoaded] = useState(false);
+
+    if (user.current && role.current === allowedRoles) {
+        return <Outlet />
+    } else if (user.current && isRoleLoaded && (role.current !== allowedRoles)) {
+        return <Forbidden />
+    } else {
+        if (isUserLoaded) {
+            if (user.current) {
+                getUserRole(user.current.uid)
+                .then((res) => {
+                    role.current = res.role;
+                    setIsRoleLoaded(true);
+                })
+                .catch (
+                    // set error if getting role fail
+                )
+            } else {
+                return <Navigate to="login" />
+            }
+        } else {
+            const { currentUser } = useAuth();
+            user.current = currentUser;
+            setIsUserLoaded(true);
+        }
     }
-
-    console.log(currentUser.roles);
-    useEffect(() => {
-        getUserRole(userId)
-            .then((res) => {
-                setCurrentUserRole(x => x = res.role);
-                console.log(res.role);
-                // console.log('res role from function', res.role);
-                console.log('res role from setter', currentUserRole);
-            });
-    }, [currentUserRole]);
-
-    // console.log('allowedRoles', allowedRoles);
-    console.log('currentUserRole', currentUserRole);
-
-    return (
-        currentUser && (allowedRoles === currentUserRole) ? <Outlet /> : <Navigate to="/login" />
-    );
 }
