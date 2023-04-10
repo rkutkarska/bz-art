78b98d22a15f89e032b6ebba6aefb16a41d2c018
rkutkarska <ralena.dimitrova@gmail.com>
2023-04-10 22:00:05 +0300

    Style forbidden component

--- src/components/Forbidden/Forbidden.js
+++ src/components/Forbidden/Forbidden.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
+import styles from './Forbidden.module.css';
 
 export const Forbidden = () => {
     return (
-        <div className="container">
-            {/* TODO style this component */}
-            <h2>403: Forbiden! You are unauthorized!</h2>
+        <div class="container">
+            <img className={styles["img-center"]} src={require("../../img/403/403.png")} alt="403 Page not found!" />
+            <h1>Forbidden! You are not authorized!</h1>
+            <div className={styles["text-center"]}>
+                <Link to="/" className="button purple">Към началната страница</Link>
+            </div>
         </div>
     );
 }
