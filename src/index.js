3ec1f3108d893387756b2ade255316b04732f48d
Ralena Kutkarska <ralena.dimitrova@gmail.com>
2023-12-07 23:30:45 +0200

    Remove modal context

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from "./App";
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import { ModalProvider } from './context/ModalContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
   <BrowserRouter>
-    <ModalProvider>
       <App />
-    </ModalProvider>
   </BrowserRouter>
   // </React.StrictMode>
 );
