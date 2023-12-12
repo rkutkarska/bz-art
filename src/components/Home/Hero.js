23a1a14c85dc2af00d636125c3519720e09b0b0f
Ralena Kutkarska <ralena.dimitrova@gmail.com>
2023-12-12 13:39:18 +0200

    Added link to current promotion

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -7,7 +7,7 @@ export const Hero = () => {
             <div className="hero__caption">
                 <div className="caption__container">
                     <p className="caption__text">50% отстъпка на бижута с естествени камъни!</p>
-                    <Link to="" className="button white same-size">Разгледай</Link>
+                    <Link to="/current-promotion" className="button white same-size">Разгледай</Link>
                 </div>
             </div>
             <img className="hero__image"src={require("../../img/index-banner.jpg")} alt="jewellery" />
